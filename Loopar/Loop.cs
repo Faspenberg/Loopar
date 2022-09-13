8b5f08706c7be4892806a97639b2426145d7dc4a
aspen <aspen@LAPTOP-N1QIQEQ8>
2022-09-13 13:11:02 +0200

    Add project files.

---
+++ Loopar/Loop.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loopar
+{
+    internal class Loop
+    {
+        public void Run()
+        {
+
+            //Case 6
+
+            //List<decimal> Temperature = new List<decimal>();
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Put temperature: ");
+            //    decimal Temp = Convert.ToDecimal(Console.ReadLine());
+
+            //    Temperature.Add(Temp);
+
+            //    while (Temperature.Count > 3)
+            //    Temperature.RemoveAt(0);
+
+            //    decimal equals = 0;
+
+            //    for (int i = 0; i < Temperature.Count; i++)
+            //    {
+            //        Console.WriteLine("Middle: " + equals / Temperature.Count);
+            //    }
+            
+
+            //}
+
+            decimal earliest = 0;
+            decimal latest = 0;
+            decimal middle = 0;
+            decimal count = 0;
+            
+            while(true)
+            {
+                Console.WriteLine("Write temperature: ")
+                    decimal temp = Convert.ToDecimal(Console.ReadLine());
+                if ()
+                }
+
+
+            }
+
+            
+            
+            //case 5
+
+            //Console.WriteLine("Put in a number: ");
+            //int input = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = input; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadKey();
+
+            //Console.WriteLine("Put in a number: ");
+            //int input = Convert.ToInt32(Console.ReadLine());
+
+            //while(input > 0)
+            //{
+            //    Console.WriteLine(input--);
+
+            //}
+
+
+
+            //case 4
+
+            //int sum = 0;
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    Console.WriteLine("Put in a number: ");
+            //    int tal = Convert.ToInt32(Console.ReadLine());
+
+            //    sum += tal;
+            //    Console.WriteLine("Equals : " + sum);
+            //}
+
+            //int sum = 0;
+            //int number = 1;
+            //while(number <= 10)
+            //{
+            //    Console.WriteLine("Put in a number: ");
+            //    int tal = Convert.ToInt32(Console.ReadLine());
+
+            //    sum += tal;
+
+            //    number = number++;
+            //    Console.WriteLine("Equals: " + sum);
+            //}
+
+
+            //Console.ReadKey();
+
+
+            //Case 3
+
+            //    while (true)
+            //        Console.WriteLine("Put in first number: ");
+            //    int tal1 = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Put in second number: ");
+            //    int tal2 = Convert.ToInt32(Console.ReadLine());
+
+            //    int sum = tal1 + tal2;
+            //    Console.WriteLine("Summan av talen är: " + tal1 + tal2);
+
+            //    Console.WriteLine("Would you like to continue? Y/N?: ");
+
+            //    string answer;
+            //    while (true)
+            //        answer = Console.ReadLine();
+            //    answer = answer.ToLower();
+
+            //    if (answer == "Y")
+            //    {
+            //        continue;
+            //    }
+            //    else if (answer == "N")
+            //        break;
+            //}
+
+
+
+            //Case 2
+
+            //Console.WriteLine("Put in first number: ");
+            //int tal1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Put in second number: ");
+            //int tal2 = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = tal1+1; i < tal2; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine("Put in first number: ");
+            //int tal1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Put in second number: ");
+            //int tal2 = Convert.ToInt32(Console.ReadLine());
+
+            //while (tal1 < tal2)
+            //{
+            //    Console.WriteLine(tal1++);
+
+
+            //}
+            //Console.ReadKey();
+
+            //Console.WriteLine("Put in first number: ");
+            //int tal1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Put in second number: ");
+            //int tal2 = Convert.ToInt32(Console.ReadLine());
+
+            //while (true)
+            //{
+            //    if (tal1 < tal2)
+            //    {
+            //        Console.WriteLine(tal1++);
+            //    }
+            //    else if (tal2 < tal1)
+            //        {
+            //        Console.WriteLine(tal2--);
+
+            //    }
+            //    Console.ReadKey();
+
+
+            //Case 1 
+
+            //for   (int i = 0; i < 10; i++)
+            //  {
+            //      Console.WriteLine(i++);
+            //  }
+            //  Console.ReadKey();
+
+            //int count = 0;
+
+            //while (count <= 10)
+            //{
+            //    Console.WriteLine(count++);              
+            //}
+        }
+    }
+}
+
+    
+
+
